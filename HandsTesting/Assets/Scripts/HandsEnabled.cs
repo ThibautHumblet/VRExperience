8fdbd9562a57ace2c5ad065bed91e60ee5960a32
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-12-04 00:26:44 +0100

    Added hand models

---
+++ HandsTesting/Assets/Scripts/HandsEnabled.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandsEnabled : MonoBehaviour
+{
+    public GameObject HandModelPrefab;
+    private GameObject spawnedHandModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedHandModel = Instantiate(HandModelPrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnedHandModel.SetActive(true);
+    }
+}
