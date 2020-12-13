fc6372018f93062aa825a87f10fbd3531c1d4c36
DriesBe <55529529+DriesBe@users.noreply.github.com>
2020-12-13 21:26:06 +0100

    Basic Teleport scripts
    Moeten nog aangepast worden aan Hands en Camera Rig.

---
+++ HandsAndVideo/Assets/Scripts/TeleportContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportContact : MonoBehaviour
+{
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+
+    public void Teleport()
+    {
+        this.gameObject.transform.position = new Vector3(x, y, z);
+    }
+}
